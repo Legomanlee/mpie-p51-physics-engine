2ebf93180fff7ee03ba05f18b75dc1813ee68237
Legomanlee <146097726+Legomanlee@users.noreply.github.com>
2024-10-22 13:21:56 +0100

    Launch a ball at pins #BOWLING

--- Assets/Bowling Alley Assets/NewBehaviourScript.cs
+++ Assets/Bowling Alley Assets/NewBehaviourScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float power = 0;
+    public float spin = 0;
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+
+        if(Input.GetKey("w")){
+            if (power <= 1500){
+                power = power + 1;
+            }
+        }
+
+        if(Input.GetKey("s")){
+            if (power >= 1){
+                power = power - 1;
+            }
+        }
+
+        if(Input.GetKey("q")){
+            if (spin >= -180){
+                spin = spin + 1;
+            }
+        }
+
+        if(Input.GetKey("e")){
+            if (spin <= 180){
+                spin = spin - 1;
+            }
+        }
+
+        if(Input.GetKey("space")){
+            Rigidbody rb = GetComponent<Rigidbody> ();
+            rb.AddForce(new Vector3 (0.0f, 0.0f, -power * 0.01f));
+        }
+
+        float angle = Input.GetAxis("Horizontal");
+        float newAngle = angle * Time.deltaTime;
+
+        Transform t = gameObject.transform;
+        t.Translate (-newAngle * 0.25f, 0.0f, 0.0f);
+        t.Rotate (spin * 0.01f, 0.0f, spin * 0.01f);
     }
 }
